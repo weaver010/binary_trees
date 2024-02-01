f00de61e8694fbe6a251f2caaf98c74f109c3ae1
weaver010 <abdelrahmannagaty839@gmail.com>
2024-02-01 11:27:01 -0800

    my life

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,28 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height -get height of a binary tree
- * @tree: tree
- * Return: height
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-        size_t r, l;
-
-        if (!tree || (!(tree->left) && !(tree->right)))
-        {
-                return (0);
-        }
-        r = binary_tree_height(tree->right);
-        l = binary_tree_height(tree->left);
-        if (l > r)
-        {
-                r = l;
-        }
-        return (r + 1);
-}
-
-/**
  * binary_tree_balance - get the balance factor
  * @tree: tree
  *
@@ -38,4 +16,25 @@ int binary_tree_balance(const binary_tree_t *tree)
 	}
 	b_f = binary_tree_height(tree->left) - binary_tree_height(tree->right);
 	return (b_f);
+}
+
+/**
+ * binary_tree_height -get height of a binary tree
+ * @tree: tree
+ * Return: height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t r, l;
+if (!tree || (!(tree->left) && !(tree->right)))
+{
+return (0);
+}
+r = binary_tree_height(tree->right);
+l = binary_tree_height(tree->left);
+if (l > r)
+{
+r = l;
+}
+return (r + 1);
 }
