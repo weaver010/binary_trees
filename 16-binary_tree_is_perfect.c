06e2a7599e69817aea74dccf78b92e0123aca2c0
weaver010 <abdelrahmannagaty839@gmail.com>
2024-02-01 12:20:12 -0800

    my life

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,14 +1,13 @@
 #include "binary_trees.h"
 
-
 /**
- * binary_tree_is_perfect - Checks if a tree is perfect
- * @tree: tree
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree:tree
  * Return: 1
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int l, h, i;
+	int l, h, p;
 
 	if (!tree)
 	{
@@ -17,9 +16,28 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	l = binary_tree_leaves(tree);
 	h = binary_tree_height(tree);
-	for (i = 1; h; h--)
-		i *= 2;
-	return ((i == l) ? 1 : 0);
+	for (p = 1; h; h--)
+		p *= 2;
+	return ((p == l) ? 1 : 0);
+}
+
+
+/**
+ * binary_tree_leaves - get  the number of  leaves
+ * @tree:tree
+ * Return: Number of leaves
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+	{
+		return (0);
+	}
+	if (!(tree->left) && !(tree->right))
+	{
+		return (1);
+	}
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
 
 
@@ -43,21 +61,4 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		r = l;
 	}
 	return (r + 1);
-}
-/**
- * binary_tree_leaves - get  the number of  leaves
- * @tree:tree
- * Return: Number of leaves
- */
-size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-	if (!tree)
-	{
-		return (0);
-	}
-	if (!(tree->left) && !(tree->right))
-	{
-		return (1);
-	}
-	return (binary_tree_leaves(tree->right) + binary_tree_leaves(tree->left));
 }
